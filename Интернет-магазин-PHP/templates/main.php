39f751ba43b6ea083d5dac1b8366d767d276dc5d
Vetl-off0098 <vetl-off@mail.ru>
2019-09-20 12:25:01 +0800

    PHP

---
+++ Интернет-магазин-PHP/templates/main.php
@@ -0,0 +1,11 @@
+<div id="promo">
+    <h1 id="promoText">
+        Новый PhoneBrand - это лучший смартфон
+    </h1>
+</div>
+
+<div id="mainTextWrap">
+    <div id="mainText">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus laboriosam, excepturi veniam repellat. Architecto itaque quia doloremque ratione a fugiat numquam provident temporibus quidem dolore eligendi necessitatibus, in autem deleniti asperiores consequuntur, deserunt quasi quam voluptatum tempore id voluptas aliquid perspiciatis! Quae repellat, laboriosam aliquid voluptatum! Aliquid aut sunt in esse cupiditate quibusdam vitae aliquam totam. Magni et ut numquam quibusdam laboriosam dicta omnis praesentium, aspernatur hic cum quae, eius, iure nobis consequatur alias tempora odit nostrum blanditiis laborum dolore.
+    </div>
+</div>
